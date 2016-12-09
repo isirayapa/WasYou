46ba675ada85d4c85a5eac894369d0c1633ebe57
isirayapa <isirayapa@gmail.com>
2016-12-09 20:27:41 +0530

    Changed database

--- SQL/database.sql
+++ SQL/database.sql
@@ -2,12 +2,11 @@ CREATE DATABASE washyou;
 USE washyou;
 
 CREATE TABLE customer(
-id INT(10) NOT NULL PRIMARY KEY,
 name VARCHAR(40),
 district VARCHAR(15),
 address VARCHAR(30),
 contact_no INT(12),
-email VARCHAR(20),
+email VARCHAR(100) NOT NULL PRIMARY KEY,
 password VARCHAR(18),
 frequency INT(10)
 );
@@ -17,19 +16,19 @@ full_name VARCHAR(200),
 vehicle_no VARCHAR(200),
 license_no VARCHAR(200) NOT NULL PRIMARY KEY,
 mobile VARCHAR(200),
-address VARCHAR(200),
+address VARCHAR(200)
 );
 
-CREATE TABLE order(
+CREATE TABLE ODR(
 id INT(10) NOT NULL PRIMARY KEY,
-customer_email INT(10),
+customer_email VARCHAR(100),
 product_capacity INT(10),
 delivery_date DATE,
 pickup_date DATE,
-state VARCHAR(10),
-license_no VARCHAR(50),
+token VARCHAR(10),
+license_no VARCHAR(200),
 CONSTRAINT fk FOREIGN KEY (customer_email) REFERENCES customer(email),
-CONSTRAINT fk2 FOREIGN KEY (license_no) REFERENCES delivery(license_no)
+CONSTRAINT fk2 FOREIGN KEY (license_no) REFERENCES driver(license_no)
 
 );
 
